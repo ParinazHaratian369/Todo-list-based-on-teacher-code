3b53d09e6d260ff94906ca6bc4e6d86ec4d9050c
Parinaz <haratian.p@gmail.com>
2025-03-03 06:01:28 +0330

    defining a function to load the json file consisting tasks

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from customtkinter import *
+import json
 
-database = "tasks.json"
+def load_tasks():
+    try:
+        with open(path, "r") as f:
+            return json.load(f)
+    except:
+        tasks = {}
+        with open(path, "w") as f:
+            json.dump(tasks, f, indent=4) 
+
+path = "tasks.json"
+tasks = load_tasks()
